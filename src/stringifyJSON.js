88376256195bfb190c17ed0c2af20b82e81b8918
Minh <minhvn29@gmail.com>
2019-11-17 18:55:19 -0800

    Add alternate refactor of entire code

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,5 +1,51 @@
 
 var stringifyJSON = function(obj) {
+  // ALTERNATE CODE FOR FUN
+  // if (obj === null) {
+  // 	return 'null';
+  // }
+
+  // if (typeof obj === 'boolean') {
+  // 	if (obj) {
+  //     return 'true';
+  //   } else {
+  //     return 'false'; 
+  //   }
+  // }
+
+  // if (typeof(obj) === 'undefined' || typeof(obj) === 'function') {
+  // 	return '';
+  // }
+
+  // if (typeof(obj) === 'string') {
+  // 	return '"' + obj + '"';
+  // }
+
+  // if (Array.isArray(obj)) {
+  //   var stringArr = [];
+
+  //   obj.forEach(function(element){
+  //     stringArr.push(stringifyJSON(element));
+  //   })
+  //   return '[' + stringArr.toString() + ']';
+  // }
+
+  // if (obj && typeof(obj) === 'object') {
+  //   var objString = '';
+  //   var keys = Object.keys(obj);
+  //   keys.forEach(function(key) {
+  //   	if (typeof(key) === 'function' || typeof(key) === 'undefined' || typeof(obj[key]) === 'function' || typeof(obj[key]) === 'undefined') {
+  //   		return '';
+  //   	} else {
+  //         objString += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+  //   	}
+  //   })
+  //   return '{' + objString.slice(0, objString.length - 1) + '}';
+  // }
+
+  // return String(obj);
+
+
   var final = '';
 
   if (Array.isArray(obj)) {
@@ -7,11 +53,11 @@ var stringifyJSON = function(obj) {
 
     obj.forEach(function(element){
       stringArr.push(stringifyJSON(element));
-    })
+    });
     return "[" + stringArr.toString() + "]";
   } else if (typeof obj === 'boolean') {
     if (obj) {
-      final +='true';
+      final += 'true';
     } else {
       final += 'false';
     }
