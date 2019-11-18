e7f9dd45a1ffe6deb67569b9e11d53c6674433e3
Minh <minhvn29@gmail.com>
2019-11-17 16:09:46 -0800

    Create var for object keys and [] for props

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,21 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
+// console.log(JSON.stringify({ x: 5, y: 6 }));
+// expected output: "{"x":5,"y":6}"
+
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // stubs
+  // input: object
+  // output: string complete with {} and quotes around the object key
+  // ie "{'objectkey': property , 'objectkey2': property2 }"
+  var objectKeys = Object.keys(obj);
+  var propertiesArray = [];
+  for (var i = 0; i < objectKeys.length; i++) {
+  	
+  }
+
 };
+
